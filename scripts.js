81edf741cddc10e290787d798318da840f1a7efd
MartinDePascali <martin.depascali@davinci.edu.ar>
2024-05-01 19:29:36 -0300

    feat: Mostrar imagenes de los pokemon

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
 const URL_ENDPOINT = 'https://pokeapi.co/api/v2/';
 const URL_POKEMON = URL_ENDPOINT + 'pokemon?limit=100&offset=0';
+const URL_POKEMON_LIST = URL_ENDPOINT + 'pokemon';
+const URL_IMAGEN = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 const mostrarLista = (pokemon) => {
     const contenedor = document.getElementById("ListaContenedor");
     const li = document.createElement('li');
 
+    console.log(pokemon);
+    console.log(pokemon.url.split('/')[6]);
+
+    const id = pokemon.url.split('/')[6];
+
     const nombre = document.createElement('h2');
     nombre.innerText = pokemon.name;
     li.appendChild(nombre);
+
+    const imagen = document.createElement('img');
+    imagen.setAttribute('src', URL_IMAGEN + id + '.png');
+    imagen.setAttribute('alt', pokemon.name);
+    li.appendChild(imagen);
 
     contenedor.appendChild(li);
 }
