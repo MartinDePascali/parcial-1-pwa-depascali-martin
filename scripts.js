dde5559cd74bb7bc6ff0add9400b36a4492fa5e5
MartinDePascali <martin.depascali@davinci.edu.ar>
2024-05-01 16:38:31 -0300

    feat: contenedor en html  y mostrar pokemon de la api

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const URL_ENDPOINT = 'https://pokeapi.co/api/v2/';
+const URL_POKEMON = URL_ENDPOINT + 'pokemon';
+
+const mostrarLista = (pokemon) => {
+    const contenedor = document.getElementById("ListaContenedor");
+    const li = document.createElement('li');
+
+    const nombre = document.createElement('h2');
+    nombre.innerText = pokemon.name;
+    li.appendChild(nombre);
+
+    contenedor.appendChild(li);
+}
+
+fetch(URL_POKEMON)
+.then(data => data.json())
+.then(result => {
+    const results = result.results.slice(0,80);
+    results.forEach(element =>{
+        mostrarLista(element);
+    });
+})
