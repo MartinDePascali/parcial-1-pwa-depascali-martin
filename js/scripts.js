d959fffa3e31dcca2d499f9f0f53e83cf4a44d3f
MartinDePascali <martin.depascali@davinci.edu.ar>
2024-05-03 03:27:55 -0300

    feat: Historial y agregados css

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ const URL_POKEMON = URL_ENDPOINT + 'pokemon?limit=100&offset=0';
 const URL_POKEMON_LIST = URL_ENDPOINT + 'pokemon';
 const URL_IMAGEN = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
+const guardarPokemon = (pokemon) => {
+    const datos = JSON.parse(localStorage.getItem("historial")) || [];
+    datos.push(pokemon);
+    localStorage.setItem("historial", JSON.stringify(datos));
+}
+
 const mostrarLista = (pokemon) => {
     const contenedor = document.getElementById("ListaContenedor");
     const li = document.createElement('li');
@@ -11,6 +17,9 @@ const mostrarLista = (pokemon) => {
 
     li.addEventListener('click', () => {
         mostrarDetalles(id);
+
+        console.log(pokemon);
+        guardarPokemon(pokemon);
     })
 
     const nombre = document.createElement('h2');
@@ -29,6 +38,30 @@ const mostrarDetalles = (id) =>  {
     window.location.href = "http://127.0.0.1:5501/detalle.html?id=" + id;
 }
 
+const mostrarPokemonHistorial = (item) => {
+    // const listaHistorial = document.querySelector('.historial');
+    // const tarjetaHistorial = document.createElement('p');
+    // tarjetaHistorial.className = "item";
+    // tarjetaHistorial.innerText = JSON.stringify(item.name);
+    // listaHistorial.appendChild(tarjetaHistorial);
+
+    const listaHistorial = document.querySelector('.historial');
+    const id = item.url.split('/')[6];
+    const imagen = document.createElement('img');
+    imagen.setAttribute('src', URL_IMAGEN + id + '.png');
+    imagen.setAttribute('alt', item.name);
+    imagen.setAttribute('class', 'imagen-historial');
+    listaHistorial.appendChild(imagen);
+}
+
+const mostrarHistorial = () => {
+    const datos = JSON.parse(localStorage.getItem("historial")) || [];
+
+    datos.forEach(item => {
+        mostrarPokemonHistorial(item);
+    })
+}
+
 fetch(URL_POKEMON)
 .then(data => data.json())
 .then(result => {
@@ -37,3 +70,5 @@ fetch(URL_POKEMON)
         mostrarLista(element);
     });
 })
+
+mostrarHistorial();
